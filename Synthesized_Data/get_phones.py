955ee1aa230d5e72709c28212c752014e51f07b6
Akshat <akshat571995@gmail.com>
2020-08-04 23:31:23 -0400

    edit synthesized data

--- Synthesized_Data/get_phones.py
+++ Synthesized_Data/get_phones.py
@@ -13,9 +13,11 @@ STORE_LOCATION = '/home/akshatgu/Intents-Analysis/Phone_Data'
 os.chdir(data_location)
 files = os.listdir()
 files = removeDS(files)
+files.sort()
 
 phones = []
 for audio in files:
+    print(audio)
     result = subprocess.check_output('python3 -m allosaurus.run --lang hin -i ' + audio, shell=True)
     result_list = result.split()
     phones.append(result_list)
