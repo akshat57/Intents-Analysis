79177e82e84a464c300729ef3d86db5bfd80b89b
Akshat <akshat571995@gmail.com>
2020-08-17 21:23:13 -0400

    Google Cloud TTS

---
+++ TaskMasterData/TTS/test.py
@@ -0,0 +1,46 @@
+from contact_tts import do_tts
+import random
+import pickle
+
+def save_data(filename, data):
+    a_file = open(filename, "wb")
+    pickle.dump(data, a_file)
+    a_file.close()
+
+
+
+language_code = 'en-US'
+voices = ['en-US-Standard-B', 'en-US-Standard-C', 'en-US-Standard-D', 'en-US-Standard-E']
+
+#Read text
+a_file = open( '../intent_text.txt', "r")
+text = a_file.readlines()
+a_file.close()
+
+#Creating index to text dictionary
+utterances= {}
+for utt in text:
+        index = utt.split('|')[0]
+        u = utt.split('|')[1][:-1]
+        utterances[index] = u
+
+total_utt = len(list(utterances.keys()))
+all_keys = list(utterances.keys())
+
+#Creating index to voice name dictionary
+index_to_voice = {}
+while len(all_keys) != 0:
+    key = random.choice(all_keys)
+    all_keys.remove(key)
+    index_to_voice[key] = random.choice(voices)
+
+#Sending for TTS
+for key in utterances:
+    text = utterances[key]
+    output_file = key
+    voice_name = index_to_voice[key]
+    do_tts(text, output_file, language_code, voice_name)
+    break
+#print(index_to_voice)
+
+save_data('index_to_voice.pkl', index_to_voice)
