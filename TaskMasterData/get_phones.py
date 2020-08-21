0ac09a56fb19461ae8491faae980dccf1347d070
Akshat <akshat571995@gmail.com>
2020-08-20 23:47:57 -0400

    gujarati

--- TaskMasterData/get_phones.py
+++ TaskMasterData/get_phones.py
@@ -8,7 +8,7 @@ def removeDS(array):
     return array
 
 info_location = '/home/akshatgu/TaskMaster/'
-data_location = '/home/akshatgu/TaskMaster/google_translate/'
+data_location = '/home/akshatgu/TaskMaster/translated_gujarati/'
 #data_location = '/home/akshatgu/TaskMaster/google_TTS_US_ENG/'
 STORE_LOCATION = '/home/akshatgu/Intents-Analysis/TaskMasterData' 
 
@@ -26,7 +26,7 @@ for i, audio in enumerate(files):
     #index = audio.split('_')[1].split('.')[0]
     index = audio.split('.')[0]
     print(index)
-    result = subprocess.check_output('python3 -m allosaurus.run --lang hin -i ' + data_location + audio, shell=True)
+    result = subprocess.check_output('python3 -m allosaurus.run --lang guj -i ' + data_location + audio, shell=True)
     result_list = result.split()
     phones_list = [phone.decode('utf-8') for phone in result_list]
     if index_to_intents[index] in all_phones: 
@@ -34,10 +34,9 @@ for i, audio in enumerate(files):
     else:
         all_phones[index_to_intents[index]] = [phones_list]
 
-
 #Save data
 os.chdir(STORE_LOCATION)
 #a_file = open("data_taskmaster_google_tts_USeng.pkl", "wb")
-a_file = open("data_taskmaster_hindi.pkl", "wb")
+a_file = open("data_taskmaster_gujarati.pkl", "wb")
 pickle.dump(all_phones, a_file)
 a_file.close()
